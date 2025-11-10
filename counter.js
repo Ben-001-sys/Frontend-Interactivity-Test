31bce9b33429ee78512f8cc801a5ae85b03d6d5f
Hotor Benjamin <hotorben001@gmail.com>
2025-11-10 16:59:01 +0000

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+
+increaseBtn.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = count;
+});
+
+decreaseBtn.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    countDisplay.textContent = count;
+  }
+});
